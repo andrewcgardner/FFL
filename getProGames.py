6176025cec1a11be49c2f7e89b75861ba1679f49
Andrew Gardner <agardner50@gmail.com>
2018-11-27 08:12:02 -0500

    overwrite local changes

--- getProGames.py
+++ getProGames.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 from datetime import datetime
-import numpy as np
+import requests
 
 #seasons = [2017,2018]
 #weeks = [1,2]
@@ -13,15 +13,26 @@ import numpy as np
 
 seasonId = 2018
 weekId = 1
-targetFile = "C:\\workspace\\python\\projects\\ffl\\new\\fullData\\proGames" + str(seasonId) + "-" + str(weekId) + ".txt"
-with open(targetFile, 'r') as tF:
-    data = json.load(tF)
+#targetFile = "C:\\workspace\\python\\projects\\ffl\\new\\fullData\\proGames" + str(seasonId) + "-" + str(weekId) + ".txt"
+#with open(targetFile, 'r') as tF:
+#    data = json.load(tF)
 
-games = pd.DataFrame.from_dict(data['progames']['games'])
+def api(endpoint,leagueId,seasonId,scoringPeriodId):
+    url = "http://games.espn.com/ffl/api/v2/" + endpoint
+    params = {'leagueId': leagueId
+        ,'seasonId': seasonId
+        ,'scoringPeriodId': scoringPeriodId
+    }
+    return requests.get(url,params=params,cookies=None).json()
 
-games['date'], games['time'] = games['gameDate'].str.split('T').str
-games['time'] = games['time'].str.split('.').str[0]
-games['gameDate'] = pd.to_datetime(games['date'] + ' ' + games['time'])
-games.drop(['date','time','status'],axis=1,inplace=True)
 
-print(games)
+data = api(endpoint='proGames',leagueId=180704,seasonId=2018,scoringPeriodId=11)
+
+def parseProGames(json):
+    games = pd.DataFrame.from_dict(json['progames']['games'])
+    games['date'], games['time'] = games['gameDate'].str.split('T').str
+    games['time'] = games['time'].str.split('.').str[0]
+    games['gameDate'] = pd.to_datetime(games['date'] + ' ' + games['time'])
+    games.drop(['date','time','status'],axis=1,inplace=True)
+
+    return games
